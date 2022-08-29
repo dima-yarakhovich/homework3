c6e9a8dd722fc615ffdffe87dd50bc9601d38c70
dima-yarakhovich <dima020490@tut.by>
2022-08-29 22:35:59 +0300

    hw3.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-
+        int a=5;
+        System.out.println("Значение переменной a с типом int  равно " + a);
+        byte b=120;
+        System.out.println("Значение переменной b с типом byte  равно " + b);
+        short c=1230;
+        System.out.println("Значение переменной c с типом short  равно " + c);
+        long d=536_111;
+        System.out.println("Значение переменной d с типом long  равно " + d);
+        float e=32.356f;
+        System.out.println("Значение переменной e с типом float  равно " + e);
+        double g=0.785263444;
+        System.out.println("Значение переменной g с типом double  равно " + g);
     }
 }
